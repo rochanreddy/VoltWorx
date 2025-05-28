e559500e2870e2f92cfd6cd8d4a86f44c3208043
rochanreddy <ayireddyrochanreddy123@gmail.com>
2025-05-29 02:32:01 +0530

    Initial commit

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -8,15 +8,35 @@ interface LoadingSpinnerProps {
 function LoadingSpinner({ size = 'medium', className }: LoadingSpinnerProps) {
   return (
     <div className={cn(
-      'animate-spin rounded-full border-t-transparent',
+      'relative',
       {
-        'h-4 w-4 border-2': size === 'small',
-        'h-8 w-8 border-4': size === 'medium',
-        'h-12 w-12 border-4': size === 'large',
+        'h-4 w-4': size === 'small',
+        'h-8 w-8': size === 'medium',
+        'h-12 w-12': size === 'large',
       },
-      'border-primary-600',
       className
-    )} />
+    )}>
+      {/* Blue ring */}
+      <div className={cn(
+        'absolute inset-0 rounded-full border-2 border-blue-500/30',
+        {
+          'border-2': size === 'small',
+          'border-3': size === 'medium',
+          'border-4': size === 'large',
+        }
+      )} />
+      
+      {/* Rotating purple spinner */}
+      <div className={cn(
+        'absolute inset-0 rounded-full animate-spin',
+        {
+          'border-2': size === 'small',
+          'border-3': size === 'medium',
+          'border-4': size === 'large',
+        },
+        'border-t-purple-500 border-r-purple-500/30 border-b-purple-500/30 border-l-purple-500/30'
+      )} />
+    </div>
   );
 }
 
