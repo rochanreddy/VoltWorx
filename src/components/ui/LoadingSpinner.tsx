471591e9a496bd5ce31f813b7800a2dc3813b4c5
rochanreddy <ayireddyrochanreddy123@gmail.com>
2025-05-29 02:26:38 +0530

    Initial commit

--- src/components/ui/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner.tsx
@@ -8,47 +8,15 @@ interface LoadingSpinnerProps {
 function LoadingSpinner({ size = 'medium', className }: LoadingSpinnerProps) {
   return (
     <div className={cn(
-      'relative',
+      'animate-spin rounded-full border-t-transparent',
       {
-        'h-4 w-4': size === 'small',
-        'h-8 w-8': size === 'medium',
-        'h-12 w-12': size === 'large',
+        'h-4 w-4 border-2': size === 'small',
+        'h-8 w-8 border-4': size === 'medium',
+        'h-12 w-12 border-4': size === 'large',
       },
+      'border-primary-600',
       className
-    )}>
-      {/* Outer spinning ring */}
-      <div className={cn(
-        'absolute inset-0 rounded-full border-t-transparent animate-spin',
-        {
-          'border-2': size === 'small',
-          'border-4': size === 'medium',
-          'border-4': size === 'large',
-        },
-        'border-primary-600'
-      )} />
-      
-      {/* Inner pulsing circle */}
-      <div className={cn(
-        'absolute inset-0 rounded-full animate-pulse',
-        {
-          'm-1': size === 'small',
-          'm-2': size === 'medium',
-          'm-3': size === 'large',
-        },
-        'bg-primary-600/30'
-      )} />
-      
-      {/* Center dot */}
-      <div className={cn(
-        'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full',
-        {
-          'h-1 w-1': size === 'small',
-          'h-2 w-2': size === 'medium',
-          'h-3 w-3': size === 'large',
-        },
-        'bg-primary-600'
-      )} />
-    </div>
+    )} />
   );
 }
 
