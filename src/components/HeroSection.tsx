edec22be04d38181e97513441302bbe0e022705a
rochanreddy <ayireddyrochanreddy123@gmail.com>
2025-05-30 19:47:29 +0530

    Initial commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % rotatingWords.length);
-    }, 2200); // 2.2 seconds
+    }, 1800); // 1.8 seconds
     return () => clearInterval(interval);
   }, []);
 
